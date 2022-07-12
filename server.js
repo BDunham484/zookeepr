bea68d5b834a232501516e04aa81d604fdf51745
ThereWolf <bdunham484@gmail.com>
2022-07-12 14:27:31 -0500

    adjust port

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 //import required package
 const express = require('express');
+//set environment variable
+const PORT = process.env.PORT || 3001;
 //instantiate the server
 const app = express();
 //route that front-end can request data from
@@ -53,6 +55,6 @@ app.get('/api/animals', (req, res) => {
 
 
 
-app.listen(3001, () => {
+app.listen(PORT, () => {
     console.log(`API server now on port 3001!`);
 });
